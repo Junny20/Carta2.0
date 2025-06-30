32fd2cac99e3b14a6e5eccae728038d006f8d527
Junny20 <liujohn4467@gmail.com>
2025-06-30 23:27:24 +1000

    changed router func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import FlashcardPage from "./pages/FlashcardPage";
 import SignupPage from "./pages/SignupPage";
 import CustomisePage from "./pages/CustomisePage";
 import FlashcardTestPage from "./pages/FlashcardTestPage";
-import RevisionTestPage from "./pages/RevisionTestPage"
+import RevisionTestPage from "./pages/RevisionTestPage";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <LandingPage /> },
+    { path: "/flashcards", element: <FlashcardPage /> },
+    { path: "/signup", element: <SignupPage /> },
+    { path: "/customise", element: <CustomisePage /> },
+    { path: "/flashcards/test", element: <FlashcardTestPage /> },
+    { path: "/revision/test", element: <RevisionTestPage /> },
+  ]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/flashcards" element={<FlashcardPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/customise" element={<CustomisePage />} />
-        <Route path="/flashcards/test" element={<FlashcardTestPage />}/>
-        <Route path="/revision/test" element={<RevisionTestPage />} />
-      </Routes>
-    </Router>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
   );
 }
 
