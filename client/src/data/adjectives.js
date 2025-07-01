51477051ea8ff56764fa58c3b5b6e72945af4f73
Junny20 <liujohn4467@gmail.com>
2025-07-01 18:37:50 +1000

    Added data for 407-453

--- client/src/data/adjectives.js
+++ client/src/data/adjectives.js
@@ -1,12 +1,29 @@
 const adjectives = [
-  { word: "prima", answer: "first" },
-  { word: "medio", answer: "middle" },
-  { word: "tenui", answer: "slender / fine" },
-  { word: "castum", answer: "chaste / pure" },
-  { word: "parvos", answer: "small / little" },
-  { word: "segnior", answer: "slower / more sluggish" },
-  { word: "mollibus", answer: "soft / gentle" },
-  { word: "fabrilia", answer: "craftsman-like / of the forge" }
-]
+  { word: "abactus, abacta, abactum", answer: "driven away, banished" },
+  { word: "tenuis, tenue", answer: "thin, delicate" },
+  { word: "castus, casta, castum", answer: "chaste, pure" },
+  { word: "mollis, molle", answer: "soft" },
+  { word: "arduus, ardua, arduum", answer: "steep, lofty" },
+  { word: "validus, valida, validum", answer: "strong, powerful" },
+  { word: "Aetneus, Aetnea, Aetneum", answer: "Aetnean, of Mount Etna" },
+  { word: "omnipotens, omnipotentis", answer: "all-powerful, almighty" },
+  { word: "segnis, segne", answer: "slow, sluggish" },
+  { word: "fabrilis, fabrile", answer: "belonging to a workman" },
+  { word: "imperfectus, imperfecta, imperfectum", answer: "unfinished" },
+  { word: "aquosus, aquosa -um", answer: "abounding in water" },
+  { word: "terrificus, terrifica, terrificum", answer: "terrifying" },
+  { word: "sequax, sequacis", answer: "that follows closely" },
+  { word: "volucer, volucris, volucre", answer: "winged" },
+  { word: "horrifer, horrifera, horriferum", answer: "horrible" },
+  { word: "cunctus, cuncta, cunctum", answer: "altogether" },
+  { word: "ocis, oce", answer: "swift/rapid" },
+  { word: "septimus -a -um", answer: "7 each" },
+  { word: "tenax, tenacis", answer: "holding fast" },
+  { word: "Sicanium, Sicania, Sicanium", answer: "Sicilian" },
+  { word: "rigens, rigentis", answer: "stiff, rigid" },
+  { word: "immanis, immane", answer: "huge, monstrous" },
+  { word: "spirans, spirantis", answer: "breathing, exhaling" },
+  { word: "fulgens, fulgentis", answer: "shining, gleaming" }
+];
 
 export default adjectives;
