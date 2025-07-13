f42a74773b37782a4961edc517617e875d006d5e
Junny20 <liujohn4467@gmail.com>
2025-07-13 13:18:44 +1000

    added env file

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = 'https://hylsfifdfzskofjvbmwx.supabase.co';
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh5bHNmaWZkZnpza29manZibXd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE2Mjk3NzQsImV4cCI6MjA2NzIwNTc3NH0.EsXVsr2R72wtVheECSIyd4iF5_MK66Dbccra0Tl49Zw"
+const supabaseURL = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 const client = createClient(supabaseURL, supabaseAnonKey);
-    
+
 export default client;
